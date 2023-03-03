f20b5c74f61219ffbfa13706724f5760c03c1fa7
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-03-03 14:14:19 -0500

    whiteboard challeng

---
+++ FizzBuzz-Challenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Challenge
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int answer = int.Parse(Console.ReadLine());
+            FizzBuzz(answer);
+            
+        }
+
+        public static void FizzBuzz(int answer)
+        {
+            if (answer % 3 == 0 && answer % 5 == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (answer % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+            }
+            else if (answer % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else
+            {
+                Console.WriteLine($"{answer}");
+            }
+        }
+    }
+}
